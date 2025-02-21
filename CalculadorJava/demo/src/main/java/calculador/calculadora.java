b2c8d5b499a2a57bd320894b155f0a306b4944b4
koanjazz <kohatsu2012@gmail.com>
2025-02-20 21:09:53 -0300

    First commit Java

---
+++ CalculadorJava/demo/src/main/java/calculador/calculadora.java
@@ -0,0 +1,14 @@
+package calculador;
+
+/**
+ * Hello world!
+ *
+ */
+public class calculadora 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+    
